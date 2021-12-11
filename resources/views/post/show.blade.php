19d291474e472603b46230c50f23fb8ed428eb30
Zac <a19860101@gmail.com>
2021-12-11 14:20:28 +0800

    20211211

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -5,17 +5,34 @@
 @endsection
 
 @section('main')
-    {{-- @foreach($posts as $post) --}}
-    <h1>{{$post->title}}</h1>
-    <div>
-        {{$post->content}}
+<div class="container">
+    <div class="row">
+        <div class="mx-auto col-xl-8 col-sm-10 p-3">
+            <div class="">
+                <h3>{{$post->title}}</h3>
+                <hr>
+                <div class="mb-3">
+                    建立時間:{{$post->created_at}}
+                </div>
+                <hr>
+                <div class="mb-3">
+                    {{$post->content}}
+                </div>
+                <hr>
+                <div class="mb-3">
+                    最後更新時間:{{$post->updated_at}}
+                </div>
+                <hr>
+                <a href="{{route('post.index')}}" class="btn btn-success ban-sm">文章列表</a>
+                <a href="{{route('post.edit',['id'=>$post->id])}}" class="btn btn-primary ban-sm">編輯文章</a>
+                <form action="{{route('post.destroy',['id'=>$post->id])}}" method="post" class="d-inline-block">
+                    @csrf
+                    @method('delete')
+                    <input type="submit" value="刪除" onclick="return confirm('確認刪除？')" class="btn btn-danger ban-sm">
+                </form>
+            </div>
+        </div>
     </div>
-    <a href="{{route('post.index')}}">文章列表</a>
-    <a href="{{route('post.edit',['id'=>$post->id])}}">編輯文章</a>
-    <form action="{{route('post.destroy',['id'=>$post->id])}}" method="post">
-        @csrf
-        @method('delete')
-        <input type="submit" value="刪除" onclick="return confirm('確認刪除？')">
-    </form>
-    {{-- @endforeach --}}
+</div>
+
 @endsection
