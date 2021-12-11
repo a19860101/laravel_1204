0f841422f85f18528be7df8ca773bbfe5ac2a49d
Zac <a19860101@gmail.com>
2021-12-11 14:08:53 +0800

    20211211

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -5,18 +5,28 @@ QWERTY
 @endsection
 
 @section('main')
-    <h1>文章列表</h1>
-    @foreach($posts as $post)
-    <div>
-        {{$post->title}}
+    <div class="container">
+        <div class="row">
+            <div class="mx-auto col-xl-8 col-sm-10 ">
+                <h2>文章列表</h2>
+            </div>
+            @foreach($posts as $post)
+            <div class="mx-auto col-xl-8 col-sm-10 p-3">
+                <div class="border p-3 rounded shadow-sm">
+                    <h3>{{$post->title}}</h3>
+                    <div class="mb-3">
+                        建立時間:{{$post->created_at}}
+                    </div>
+                    <div class="mb-3">
+                        {{$post->content}}
+                    </div>
+                    <div class="mb-3">
+                        最後更新時間:{{$post->updated_at}}
+                    </div>
+                    <a href="{{route('post.show',['id'=>$post->id])}}" class="btn btn-primary btn-sm">繼續閱讀</a>
+                </div>
+            </div>
+            @endforeach
+        </div>
     </div>
-    <div>
-        {{$post->content}}
-    </div>
-    <div>
-        {{$post->created_at}}
-    </div>
-    <a href="{{route('post.show',['id'=>$post->id])}}">繼續閱讀</a>
-    <hr>
-    @endforeach
 @endsection
