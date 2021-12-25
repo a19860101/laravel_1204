4cb9085235086fcdf958fb73380c960ac4c0059b
Zac <a19860101@gmail.com>
2021-12-25 16:20:55 +0800

    20211225

--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,12 @@ Route::get('/', function () {
 });
 
 Route::get('/post','PostController@index')->name('post.index');
-Route::get('/post/create','PostController@create')->name('post.create');
-Route::post('/post','PostController@store')->name('post.store');
+Route::get('/post/create','PostController@create')->name('post.create')->middleware('auth');
+Route::post('/post','PostController@store')->name('post.store')->middleware('auth');
 Route::get('/post/{id}','PostController@show')->name('post.show');
-Route::get('/post/{id}/edit','PostController@edit')->name('post.edit');
-Route::put('/post/{post}','PostController@update')->name('post.update');
-Route::delete('/post/{id}','PostController@destroy')->name('post.destroy');
+Route::get('/post/{id}/edit','PostController@edit')->name('post.edit')->middleware('auth');
+Route::put('/post/{post}','PostController@update')->name('post.update')->middleware('auth');
+Route::delete('/post/{id}','PostController@destroy')->name('post.destroy')->middleware('auth');
 Route::get('/post/category/{category}','PostController@postWithCategory')->name('post.category');
 Route::get('/post/user/{user}','PostController@postWithUser')->name('post.user');
 
