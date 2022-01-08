0ab0b2270ffe349090f0c62c78871e3564a92416
Zac <a19860101@gmail.com>
2022-01-08 12:28:58 +0800

    20220108

--- resources/views/template/tinymce.blade.php
+++ resources/views/template/tinymce.blade.php
@@ -3,6 +3,12 @@
     tinymce.init({
         selector: '#content',
         height:600,
-        language:'zh_TW'
+        language:'zh_TW',
+        plugins: 'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
+        menubar: 'file edit view insert format tools table help',
+        toolbar: 'removeformat | undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor  | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
+        toolbar_sticky: true,
+
+
     });
 </script>
